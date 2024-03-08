d3cd3f59712c71a4407fcb2fc7341b1f521cb607
freyder_montano <fremontano24@gmail.com>
2024-03-08 19:48:58 -0300

    Accion y ruta de registro de usaurio

--- controllers/user.js
+++ controllers/user.js
@@ -1,6 +1,7 @@
 //Importar dependencia y modulos
 const bcrypt = require('bcrypt');
 const User = require('../models/user');
+const user = require('../models/user');
 
 
 
@@ -15,8 +16,7 @@ const pruebaUser = (req, res) => {
 
 
 //Registrar Usuarios
-const registerUsuarios = (req, res) => {
-
+const registerUsuarios = async (req, res) => {
     //recoger los datos que llegan de la peticion
     const params = req.body;
 
@@ -27,18 +27,49 @@ const registerUsuarios = (req, res) => {
         });
     }
 
-    //Crear objeto usuario para guardar
-    let user_save = new User(params);
 
-    //Control de usuario duplicado en la base de datos
-    //exec ejecuta mi consulta en la base de datos
+    //Controlar usuario duplicados
+    try {
+        const existingUsers = await User.find({
+            $or: [
+                { email: params.email.toLowerCase() },
+                { nickname: params.nickname.toLowerCase() }
+            ]
+        }).exec();
+
+        if (existingUsers && existingUsers.length > 0) {
+            return res.status(200).json({
+                status: 'success',
+                error: 'Ya existe un usuario con ese email o nickname'
+            });
+        }
+
+        //Cifrar contraseña usuario
+        let hashedPassword = await bcrypt.hash(params.password, 10);
+
+        params.password = hashedPassword;
+
+        //Crear objeto usuario para guardar
+        let user_to_save = new User(params);
+        let save_user = await user_to_save.save();
 
 
-    return res.status(200).json({
-        status: 'success',
-        user_save
 
-    });
+        //Devolver respuesta
+        return res.status(200).json({
+            status: 'success',
+            message: 'Accion registro de usuario',
+            user_to_save: save_user
+        });
+
+    } catch (error) {
+        console.error('Error al guardar el documento:', error);
+
+        return res.status(500).json({
+            status: "Error",
+            messague: "Error en la consulta de usuarios",
+        });
+    }
 };
 
 
